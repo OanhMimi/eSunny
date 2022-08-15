07db3ff12afb627853a979723524098371faca2c
Mimi <lyoanhmymimi@gmail.com>
2022-08-14 23:43:57 -0700

    long day

--- src/scripts/game_view.js
+++ src/scripts/game_view.js
@@ -2,12 +2,13 @@ import Menu from "./menu.js";
 
 class GameView{
     constructor(params){ //[1100,680]
-        this.params = params;
-        this.dimX = params["dim"][0]; //[1100]
-        this.dimY= params["dim"][1]; //[680]
+        
         // this.menu = new Menu(params);
         const canvas = document.getElementById("game-canvas");
         const ctx = canvas.getContext("2d");
+        this.params = params;
+        this.dimX = params["dim"][0]; //[1100]
+        this.dimY= params["dim"][1]; //[680]
         // this.ctx = ctx;
         this.animateBool = true;
         this.clearScreen = false;
@@ -22,7 +23,7 @@ class GameView{
         // const sunnyVolumeButton = document.getElementById("sunny-volume-btn");
         const that = this;
         const instructionBox = document.getElementById("instructions-text");
-        instructionBox.style["background-image"] = "url(./src/assets/menu/sunny_instructions.png"
+        // instructionBox.style["background-image"] = "url(./src/assets/menu/sunny_instructions.png"
 
         const backgroundMusic = new Audio();
         backgroundMusic.src = './src/assets/audio/sunnyDay_audio.mp3'
@@ -38,15 +39,28 @@ class GameView{
 
         });
 
-        const sunnyMenuImage = new Image();
-        sunnyMenuImage.src = './src/assets/menu/shadow_dog.png';
-
+        const sunnyMenuImage = document.getElementById('shadow-dog-image')
+        // sunnyMenuImage.src = './src/assets/menu/shadow_dog.png';
+        const spriteWidth = 575;
+        const spriteHeight = 523; 
+        let frameX = 0;
+        let frameY = 0;
+        let gameFrame = 0;
+        const staggerFrames = 5;
 
         function animateSunnyMenu(){
-            ctx.clearRect(0,0,this.dimX,this.dimY); //clear the entire canvas between every animation frame
-            ctx.drawImage(sunnyMenuImage,0,0,400,400,0,0,this.dimX,this.dimY) //(imageuwanttodraw,srcx,srcy,srcwidth,srcheight,dx,dy,dw,dh)
+            ctx.clearRect(0,0,1100,680); //clear the entire canvas between every animation frame
+            ctx.drawImage(sunnyMenuImage, frameX*spriteWidth,frameY*spriteHeight,spriteWidth,spriteHeight,0,0,spriteWidth,spriteHeight); //(imageuwanttodraw,srcx,srcy,srcwidth,srcheight,dx,dy,dw,dh)
+            if (gameFrame % staggerFrames === 0){
+                if (frameX < 6) frameX++;
+                else frameX = 0;
+            }
+
+            gameFrame++;
             requestAnimationFrame(animateSunnyMenu); //build in method that runs the function we pass to it 
-        }
+        };
+
+        animateSunnyMenu();
 
 
         playButton.addEventListener("click",function(){
