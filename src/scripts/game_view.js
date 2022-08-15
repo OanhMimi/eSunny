816e5251ce27c4a1380e7e341b2867162617fa79
Mimi <lyoanhmymimi@gmail.com>
2022-08-14 22:48:56 -0700

    another commit

--- src/scripts/game_view.js
+++ src/scripts/game_view.js
@@ -6,14 +6,16 @@ class GameView{
         this.dimX = params["dim"][0]; //[1100]
         this.dimY= params["dim"][1]; //[680]
         // this.menu = new Menu(params);
-        this.ctx = ctx;
+        const canvas = document.getElementById("game-canvas");
+        const ctx = canvas.getContext("2d");
+        // this.ctx = ctx;
         this.animateBool = true;
         this.clearScreen = false;
 
         
         const playButton = document.getElementById("play-btn");
         const instructionButton = document.getElementById("instructions-btn");
-        // const volumeButton = document.getElementById("volume-btn");
+        const volumeButton = document.getElementById("volume-btn");
         const menuButton = document.getElementById("menu-btn");
         const returnMenuButton = document.getElementById("return-to-menu")
     
@@ -21,6 +23,31 @@ class GameView{
         const that = this;
         const instructionBox = document.getElementById("instructions-text");
         instructionBox.style["background-image"] = "url(./src/assets/menu/sunny_instructions.png"
+
+        const backgroundMusic = new Audio();
+        backgroundMusic.src = './src/assets/audio/sunnyDay_audio.mp3'
+
+        volumeButton.addEventListener("click",function (){
+            backgroundMusic.play();
+            backgroundMusic.addEventListener('playing', function(){
+                console.log('background music is currently playing...')
+            });
+            backgroundMusic.addEventListener('ended', function(){
+                console.log('music ended')
+            });
+
+        });
+
+        const sunnyMenuImage = new Image();
+        sunnyMenuImage.src = './src/assets/menu/shadow_dog.png';
+
+
+        function animateSunnyMenu(){
+            ctx.clearRect(0,0,this.dimX,this.dimY); //clear the entire canvas between every animation frame
+            ctx.drawImage(sunnyMenuImage,0,0,400,400,0,0,this.dimX,this.dimY) //(imageuwanttodraw,srcx,srcy,srcwidth,srcheight,dx,dy,dw,dh)
+            requestAnimationFrame(animateSunnyMenu); //build in method that runs the function we pass to it 
+        }
+
 
         playButton.addEventListener("click",function(){
             playButton.hidden = true;
@@ -63,9 +90,9 @@ class GameView{
         // })
         }
         clearCanvas() {
-            let canvas = document.getElementById("game-canvas");
-            this.ctx = canvas.getContext("2d");
-            this.ctx.clearRect(0,0,this.dimX,this.dimY);
+            // let canvas = document.getElementById("game-canvas"); //hold a reference to the actual html game-cavs element
+            // this.ctx = canvas.getContext("2d"); //access to canvas 2d methods //all custom 2d canvas methods are stored in ctx
+            ctx.clearRect(0,0,this.dimX,this.dimY); 
         }
 
         titleMenu(){
@@ -88,6 +115,9 @@ class GameView{
                 return;
               }
         }
+
+        
+   
     }
 
 
